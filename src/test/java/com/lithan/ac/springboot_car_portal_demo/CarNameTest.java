688e15f5c1201c864c28e7712e814c320a9a1a52
AyeThiriPaing <124244936+AyeThiriPaing@users.noreply.github.com>
2023-11-07 16:26:43 +0630

    Add files via upload

---
+++ src/test/java/com/lithan/ac/springboot_car_portal_demo/CarNameTest.java
@@ -0,0 +1,26 @@
+package com.lithan.ac.springboot_car_portal_demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lithan.ac.springboot_car_portal_demo.daos.CarRepository;
+import com.lithan.ac.springboot_car_portal_demo.entities.Car;
+
+@SpringBootTest
+class CarNameTest {
+
+	@Autowired
+	CarRepository carRepo;
+
+	@Test
+	void carNameTest(){
+		String make="Italy";
+		List<Car> car=carRepo.searchBymake(make);
+		assertFalse(car.isEmpty());
+	}
+}
